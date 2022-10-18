4ab9de9af1744d54e92e46c8747079f3cf9c30ed
Brian Shipe <bmshipe@gmail.com>
2022-10-18 06:01:47 -0500

    Expand tests; add base case

--- 10-LeetCode/0038-CountAndSay/count-and-say_test.go
+++ 10-LeetCode/0038-CountAndSay/count-and-say_test.go
@@ -3,16 +3,31 @@ package countandsay
 import "testing"
 
 func countAndSay(n int) string {
+	if n == 1 {
+		return "1"
+	}
 	return ""
 }
 
 // ----- testing ----- //
 func Test_CountAndSay_S001(t *testing.T) {
-	runCountAndSayTest(t, 1, "1")
+	runCountAndSayTest(t, 1, "1") // base case
 }
 
 func Test_CountAndSay_S002(t *testing.T) {
-	runCountAndSayTest(t, 4, "1121")
+	runCountAndSayTest(t, 2, "11") // one 1
+}
+
+func Test_CountAndSay_S003(t *testing.T) {
+	runCountAndSayTest(t, 3, "21") // two 1s
+}
+
+func Test_CountAndSay_S004(t *testing.T) {
+	runCountAndSayTest(t, 4, "1211") // one 2, one 1
+}
+
+func Test_CountAndSay_S005(t *testing.T) {
+	runCountAndSayTest(t, 5, "111221") // one 1, one 2, 2 ones
 }
 
 // ----- helpers ----- //
