9f6e5972069778193b900f8db6af281b4f57f57f
Brian Shipe <bmshipe@gmail.com>
2023-03-15 08:10:28 -0500

    Add helpers to build trees from a string

--- 10-LeetCode/ln/ln.go
+++ 10-LeetCode/ln/ln.go
@@ -3,6 +3,7 @@ package ln
 import (
 	"fmt"
 	"strconv"
+	"strings"
 )
 
 type ListNode struct {
@@ -36,4 +37,58 @@ func ListToString(head *ListNode) string {
 		head = head.Next
 	}
 	return str
+}
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func BuildTree(tree string) *TreeNode {
+	vals := strings.Split(tree, ",")
+	treeData := make([]*int, len(vals))
+
+	for i, val := range vals {
+		if val == "null" || val == "nil" {
+			treeData[i] = nil
+		} else {
+			data, _ := strconv.Atoi(val)
+			treeData[i] = &data
+		}
+	}
+
+	return buildTree(&treeData)
+}
+
+func buildTree(tree *[]*int) *TreeNode {
+	root := new(TreeNode)
+	root.Val = *(*tree)[0]
+
+	build(root, 0, tree)
+
+	return root
+}
+
+func build(root *TreeNode, index int, treeData *[]*int) {
+	leftIndex := 2*index + 1
+	rightIndex := 2*index + 2
+
+	if leftIndex < len(*treeData) && (*treeData)[leftIndex] != nil {
+		root.Left = new(TreeNode)
+		root.Left.Val = *(*treeData)[leftIndex]
+
+		build(root.Left, leftIndex, treeData)
+	}
+
+	if rightIndex < len(*treeData) && (*treeData)[rightIndex] != nil {
+		root.Right = new(TreeNode)
+		root.Right.Val = *(*treeData)[rightIndex]
+
+		build(root.Right, rightIndex, treeData)
+	}
+}
+
+func Int(val int) *int {
+	return &val
 }
